5ecd5b9fdb5c3e8105a35f6c970334cef2df30d4
Huy Bui <hooeybooey@MacBook-Pro.local>
2022-03-19 14:54:37 -0700

    add closestToTargetTripletSum file and update tripletSumToZero file

---
+++ two-pointers/closestToTargetTripletSum.ts
@@ -0,0 +1,60 @@
+/*
+
+Given an array of unsorted numbers and a target number, 
+find a triplet in the array whose sum is as close to the target number as possible, 
+return the sum of the triplet. 
+If there are more than one such triplet, 
+return the sum of the triplet with the smallest sum.
+
+Example 1:
+
+Input: [-2, 0, 1, 2], target=2
+Output: 1
+Explanation: The triplet [-2, 1, 2] has the closest sum to the target.
+Example 2:
+
+Input: [-3, -1, 1, 2], target=1
+Output: 0
+Explanation: The triplet [-3, 1, 2] has the closest sum to the target.
+Example 3:
+
+Input: [1, 0, 1, 1], target=100
+Output: 3
+Explanation: The triplet [1, 1, 1] has the closest sum to the target.
+
+*/
+
+const closestToTargetTripletSum = (arr: number[], target: number) => {
+  // store and track closestDiff;
+  let closestDiff: number = Infinity;
+  // sort the arr, then iterate through each num in arr
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    let currNum: number  = arr[i], 
+      left: number = i + 1, 
+      right: number = arr.length -1;
+    // establish pointers, then grab diff of target minus the triplet
+    // if diff = 0, return target
+    while (left < right) {
+      let diff = target - currNum - arr[left] - arr[right];
+      if (diff === 0) return target;
+      
+      // if abs value of diff < abs value of closest or diff > closest, reassign closest
+      let diffLessThanClosest = (Math.abs(diff) < Math.abs(closestDiff));
+      let diffsEqualCheck = ((Math.abs(diff) === Math.abs(closestDiff)) && diff > closestDiff);
+      if (diffLessThanClosest || diffsEqualCheck) closestDiff = diff;
+
+      // move left or right pointer depending on if diff greater than 0 or not
+      if (diff > 0) left++;
+      else right--;
+    }
+  }
+  return target - closestDiff;
+};
+
+console.log(closestToTargetTripletSum([-2, 0, 1, 2], 2)) //-> 1
+console.log(closestToTargetTripletSum([-3, -1, 1, 2], 1)) //-> 0
+console.log(closestToTargetTripletSum([1, 0, 1, 0, 1], 100)) //-> 3
+
+
+
