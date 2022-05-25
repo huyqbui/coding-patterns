1e512107e1272c01291eff335070ad7476bf197f
huyqbui <shuyttea@gmail.com>
2022-05-24 20:06:49 -0700

    update longest valid parenthesis function

--- string-manipulation/longestValidParenthesis.ts
+++ string-manipulation/longestValidParenthesis.ts
@@ -17,25 +17,44 @@ Example 3:
   Output: 0
 */
 
+// Time: O(n) where we iterate twice and n is length of s
+// Space: O(1) for storing the needed variables
 const longestValidParentheses = (s: string) => {
   if (!s.length) return 0;
 
-  let max = 0;
-  const stack = [-1];
-  
-  for (let i = 0; i < s.length; i++) {
-      if (s[i] === '(') {
-          stack.push(i)
-      } else {
-          stack.pop();
-          if (!stack.length) stack.push(i);
-          else max = Math.max(max, i - stack[stack.length - 1]);
-      }
+  let maxLength = 0,
+    left = 0,
+    right = 0;
+
+  // iterate forwards through string, and check each char
+  for (const char of s) {
+    if (char === '(') left++;
+    if (char === ')') right++;
+    if (left === right) maxLength = Math.max(maxLength, left + right);
+    if (right > left) {
+      left = 0;
+      right = 0;
+    }
   }
-  return max;
+
+  // reset left and right, and iterate backwards through string
+  // check each char in string, and update maxLength as needed
+  left = 0;
+  right = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    const char = s[i];
+    if (char === '(') left++;
+    if (char === ')') right++;
+    if (left === right) maxLength = Math.max(maxLength, left + right);
+    if (left > right) {
+      left = 0;
+      right = 0;
+    }
+  }
+  return maxLength;
 };
 
-console.log(longestValidParentheses('')) //-> 0
-console.log(longestValidParentheses('(()')) //-> 2
-console.log(longestValidParentheses(')()())')) //-> 4
-console.log(longestValidParentheses('()(()')) //-> 2
+console.log(longestValidParentheses('')); //-> 0
+console.log(longestValidParentheses('(()')); //-> 2
+console.log(longestValidParentheses(')()())')); //-> 4
+console.log(longestValidParentheses('()(()')); //-> 2
