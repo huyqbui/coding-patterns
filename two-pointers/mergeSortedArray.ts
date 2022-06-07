fe2e92d6b8fedaf2a9cca12a03692076a9dc54ea
huyqbui <shuyttea@gmail.com>
2022-06-06 21:18:53 -0700

    add function to merge sorted arrays

---
+++ two-pointers/mergeSortedArray.ts
@@ -0,0 +1,60 @@
+/*
+
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, 
+but instead be stored inside the array nums1. 
+To accommodate this, nums1 has a length of m + n, 
+where the first m elements denote the elements that should be merged, 
+and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+  Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+  Output: [1,2,2,3,5,6]
+  Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+  The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Example 2:
+  Input: nums1 = [1], m = 1, nums2 = [], n = 0
+  Output: [1]
+  Explanation: The arrays we are merging are [1] and [].
+  The result of the merge is [1].
+*/
+
+// Time: O(n + m) where we check each n + m elements
+// Space: O(1) constant
+const mergeArrays = (nums1: number[], m: number, nums2: number[], n: number) => {
+  // have pointers for end of nums1 & nums2
+  let p1 = m - 1;
+  let p2 = n - 1;
+
+  // use for loop to start adding numbers into nums1, starting from end of nums1 + n numbers
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) break
+
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1];
+      p1 -= 1;
+    } else {
+      nums1[i] = nums2[p2];
+      p2-= 1;
+    }
+  }
+  return nums1;
+}
+const nums1A = [1, 2, 3, 0, 0, 0]
+const mA = 3
+const nums2A = [2, 5, 6]
+const nA = 3;
+
+const nums1B = [-10,-10,-9,-9,-9,-8,-8,-7,-7,-7,-6,-6,-6,-6,-6,-6,-6,-5,-5,-5,-4,-4,-4,-3,-3,-2,-2,-1,-1,0,1,1,1,2,2,2,3,3,3,4,5,5,6,6,6,6,7,7,7,7,8,9,9,9,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+const mB = 55
+const nums2B = [-10,-10,-9,-9,-9,-9,-8,-8,-8,-8,-8,-7,-7,-7,-7,-7,-7,-7,-7,-6,-6,-6,-6,-5,-5,-5,-5,-5,-4,-4,-4,-4,-4,-3,-3,-3,-2,-2,-2,-2,-2,-2,-2,-1,-1,-1,0,0,0,0,0,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,7,7,7,7,7,7,7,8,8,8,8,9,9,9,9]
+const nB = 99
+
+console.log(mergeArrays(nums1A, mA, nums2A, nA)); //-> [1, 2, 2, 3, 5, 6]
+console.log(mergeArrays(nums1B, mB, nums2B, nB)); 
+//-> [-10, -10, -10, -10, -9, -9, -9, -9, -9, -9, -9, -8, -8, -8, -8, -8, -8, -8, -7, -7, -7, -7, -7, -7, -7, -7, -7, -7, -7, -6, -6, -6, -6, -6, -6, -6, -6, -6, -6, -6, -5, -5, -5, -5, -5, -5, -5, -5, -4, -4, -4, -4, -4, -4, -4, -4, -3, -3, -3, -3, -3, -2, -2, -2, -2, -2, -2, -2, -2, -2, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9]
